70f1d5fc8aa02746742f76b155f6b943d4e96fb5
SaketV <115332760+Saket9999@users.noreply.github.com>
2022-10-12 09:52:50 +0530

    Add files via upload

--- number.c
+++ number.c
@@ -18,27 +18,28 @@ int main()
   n=p;
   t=0;
   m=0;
-  for(int i=2;i<n;i++)
+  for(int i=1;i<n;i++)
   {
-    printf("#");
+   
     m=n%i;
     if(m==0)
     {
-      t=t+m;
+      t=t+i;
     }
   }
   if(t==n)
-    printf("%d is perfect Number",n);
+    printf("%d is perfect Number\n",n);
    m=0; 
-  for(int i=2;i<n/2;i++)
+  for(int i=2;i<=n/2;i++)
   {
     m=n%i;
     if(m!=0)
     {
-      printf("%d is Prime Number",n);
+      printf("%d is Prime Number\n",n);
+      break;
     }
     
    } 
-    
+  
     return 0;
   }  
