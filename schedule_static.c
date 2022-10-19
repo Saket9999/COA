1355b1ccd476a3df34d34cfd46f4e0004abbbfdc
SaketV <115332760+Saket9999@users.noreply.github.com>
2022-10-19 09:38:12 +0530

    Add files via upload

--- schedule_static.c
+++ schedule_static.c
@@ -7,7 +7,7 @@ int main()
   {
     //#pragma omp for schedule(static,10)
    // #pragma omp for schedule(dynamic,5)
-   #pragma omp for schedule(guided,5)
+   #pragma omp for schedule(guided,19)
     for(int i=0;i<20;i++)
     {
       printf("\niteration %d run by the thread %d",i,omp_get_thread_num());
